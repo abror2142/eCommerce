7ba4688dfc4f88a4a521533cb63083c1a64d3a67
Abrorbek Ismoilov <abror2142@gmail.com>
2024-09-16 19:54:31 +0500

    Products model and logic

--- backend/products/models.py
+++ backend/products/models.py
@@ -1,3 +1,43 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+from accounts.models import Address
+
+
+User = get_user_model()
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    parent = models.ForeignKey('self', null=True, blank=True, on_delete=models.SET_NULL)
+
+    def __str__(self):
+        return self.name
+
+
+class Vendor(models.Model):
+    user = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+    company_name = models.CharField(max_length=150, null=True, blank=True)
+    address = models.ForeignKey(Address, null=True, blank=True, on_delete=models.SET_NULL)
+
+    def __str__(self):
+        return self.user
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=150)
+    short_description = models.CharField(max_length=255)
+    full_description = models.TextField(null=True, blank=True)
+    price = models.DecimalField(max_digits=20, decimal_places=2)
+    vendor = models.ForeignKey(Vendor, null=True, on_delete=models.SET_NULL)
+
+    def __str__(self):
+        return self.name
+
+
+class ProductCategory(models.Model):
+    product = models.ForeignKey(Product, null=True, on_delete=models.SET_NULL)
+    category = models.ForeignKey(Category, null=True, on_delete=models.SET_NULL)
+
+    def __str__(self):
+        return f"{self.category}: {self.product}"
