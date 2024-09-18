2a6ffb889a12f1f846b96f536e9ce81ffae7ce9f
Abrorbek Ismoilov <abror2142@gmail.com>
2024-09-18 15:37:09 +0500

    Language box styling

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -2,62 +2,153 @@ import { useState, useRef } from "react";
 import { NavLink } from "react-router-dom"
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faMagnifyingGlass, faXmark } from "@fortawesome/free-solid-svg-icons";
+import { faMagnifyingGlass, faXmark, faCartShopping, faUser } from "@fortawesome/free-solid-svg-icons";
+import uzbekistan from "../assets/uzb.png";
+import britain from "../assets/britain.png";
+import russia from "../assets/russian.png";
 
 
 function NavBar(){
+
+    const langOptions = {
+        "eng": {
+            "flag": britain,
+            "code": "ENG" 
+        },
+        "uzb": {
+            "flag": uzbekistan,
+            "code": "UZB"
+        },
+        "ru": {
+            "flag": russia,
+            "code": "RU"
+        }
+    }
+
     const [search, setSearch] = useState('')
     const searchRef = useRef()
+    const [showLangOptions, setShowLangOptions] = useState(false)
+    const [activeLang, setActiveLang] = useState(langOptions['eng'])
+
+    console.log(showLangOptions)
 
     async function handleSubmit() {
         console.log(search)
     }
+
   
     return (
-        <nav className="nav-bar">
-            <div className="nav-left">
-                <h2>eCommerce</h2>
-            </div>
+        <> 
+            <nav className="nav-bar">
 
-            <div className="nav-center">
-                <div className="search-box">
-                    <div className="search-icon-box">
-                        <FontAwesomeIcon 
-                            icon={faMagnifyingGlass} 
-                            className="search-icon" 
-                            onClick={handleSubmit}
-                        />
-                    </div>  
-                        <input 
-                            id="search-input"
-                            className="search-input"
-                            type="text"
-                            onChange={(e) => setSearch(e.target.value)}
-                            value={search}
-                            ref={searchRef}
-                        />
-                    <div className="xmark-icon-box">
-                        {(search.length > 0) && 
+                <div className="nav-left">
+                    <ul className="brand-name">
+                        <li>e</li>
+                        <li className="sp">C</li>
+                        <li>o</li>
+                        <li>m</li>
+                        <li>m</li>
+                        <li>e</li>
+                        <li>r</li>
+                        <li>c</li>
+                        <li>e</li>
+                    </ul>
+                </div>
+
+                <div className="nav-center">
+                    <div className="search-box">
+                        <div className="search-icon-box">
                             <FontAwesomeIcon 
-                                icon={faXmark} 
-                                className="xmark-icon"
-                                onClick={() => {
-                                    searchRef.current.focus()
-                                    setSearch('')
-                                }} 
+                                icon={faMagnifyingGlass} 
+                                className="search-icon" 
+                                onClick={handleSubmit}
                             />
-                        }   
+                        </div>  
+                            <input 
+                                id="search-input"
+                                className="search-input"
+                                type="text"
+                                onChange={(e) => setSearch(e.target.value)}
+                                value={search}
+                                ref={searchRef}
+                            />
+                        <div className="xmark-icon-box">
+                            {(search.length > 0) && 
+                                <FontAwesomeIcon 
+                                    icon={faXmark} 
+                                    className="xmark-icon"
+                                    onClick={() => {
+                                        searchRef.current.focus()
+                                        setSearch('')
+                                    }} 
+                                />
+                            }   
+                        </div>
                     </div>
                 </div>
-            </div>
 
-            <div className="nav-right">
-                <div className="nav-account">
-                    <NavLink className="register" to="#">Register</NavLink>
-                    <NavLink className="login" to="#">Login</NavLink>
+                <div className="nav-right">
+                    <div 
+                        className="lang-box"
+                        onBlur={() => setShowLangOptions(false)}
+                        tabIndex={0}
+                        onMouseLeave={() => setShowLangOptions(false)}
+                    >
+                        <img 
+                            src={activeLang.flag}
+                            className="lang-img"
+                            onMouseOver={() => setShowLangOptions((prev) => !prev)}
+                        />
+                        {
+                            showLangOptions 
+                            && <div 
+                                className="lang-options-box"
+                                >
+                                {Object.values(langOptions).map((option, index) => (
+                                    <div 
+                                        key={'lang-option-' + index}
+                                        className="lang-option"
+                                        onClick={() => {
+                                            setShowLangOptions(false)
+                                            setActiveLang(option)
+                                        }} 
+                                    >
+                                        <img 
+                                            src={option.flag}
+                                            className="lang-img"
+                                        />
+                                        <p>{option.code}</p>
+                                    </div>
+                                ))}
+                               
+                            </div>
+                        }
+                    </div>
+
+                    <div className="nav-account">
+                        
+                        <NavLink className="register" to="#">
+                            <FontAwesomeIcon 
+                                icon={faUser} 
+                                className="user-icon"
+                            />
+                            Account
+                        </NavLink>
+                    </div>
+
+                    <div className="nav-cart-box">
+                        <div>
+                            <FontAwesomeIcon 
+                                icon={faCartShopping}
+                                className="cart-icon"
+                            />
+                        </div>
+                        <p>(1)</p>
+                    </div>
+
                 </div>
-            </div>
-        </nav>
+            </nav>
+        </>
     )
 }
 
--- frontend/src/containers/Header.jsx
+++ frontend/src/containers/Header.jsx
@@ -1,9 +1,19 @@
 import NavBar from "../components/NavBar";
+import "cities";
 
+
+function successFunction(position) {
+    console.log(position)
+    var lat = position.coords.latitude;
+    var long = position.coords.longitude;
+    console.log('Your latitude is :'+lat+' and longitude is '+long);
+}
 
 function Header() {
+ 
+   
     return (
-        <>
+        <> 
             <NavBar />
         </>
     )
