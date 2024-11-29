336a71ea78b6d1e09c6f1f50d2375591eb109389
Abrorbek Ismoilov <abror2142@gmail.com>
2024-11-29 11:34:54 +0500

    Product Models

--- backend/products/admin.py
+++ backend/products/admin.py
@@ -1,33 +1,90 @@
 from django.contrib import admin
-
-from .models import Category, Vendor, Product, ProductCategory, ProductImage
+from .models import (Category, Product, ProductCategory, ProductVariant, 
+                     Brand, Company, ProductColor, ProductImage, ProductImageSet, 
+                     ProductMemory, ProductSize, ProductVariantCustomCharacter, 
+                     CompanyBrand, CustomCharacter, CustomCharacterOption)
 
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ['id', 'category_name', 'parent']
-    list_display_links = ['category_name']
-
-
-@admin.register(Vendor)
-class VendorAdmin(admin.ModelAdmin):
-    list_display = ['id', 'user', 'company_name']
-    list_display_links = ['id', 'user']
+    list_display = ['id', 'name']
+    list_display_links = ['name']
 
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['id', 'product_name', 'short_description']
-    list_display_links = ['product_name']
+    list_display = ['id', 'name']
+    list_display_links = ['name']
 
 
 @admin.register(ProductCategory)
 class ProductCategoryAdmin(admin.ModelAdmin):
     list_display = ['id', 'product', 'category']
-    list_display_links = ['id']
+    list_display_links = ['product']
+
+
+@admin.register(ProductVariant)
+class ProductVariantAdmin(admin.ModelAdmin):
+    list_display = ['id','name', 'product']
+    list_display_links = ['name', 'product']
+
+
+@admin.register(Brand)
+class BrandAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name']
+    list_display_links = ['name']
+
+
+@admin.register(CompanyBrand)
+class CompanyBrandAdmin(admin.ModelAdmin):
+    list_display = ['id', 'company', 'brand']
+    list_display_links = ['company', 'brand']
+
+
+@admin.register(ProductColor)
+class CompanyAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'code']
+    list_display_links = ['name']
 
 
 @admin.register(ProductImage)
 class ProductImageAdmin(admin.ModelAdmin):
-    list_display = ['id', 'product']
-    list_display_links = ['product']
+    list_display = ['id', 'image']
+    list_display_links = ['id']
+
+
+@admin.register(ProductSize)
+class ProductSizeAdmin(admin.ModelAdmin):
+    list_display = ['id', 'size', 'roman_label']
+    list_display_links = ['size']
+
+
+@admin.register(ProductMemory)
+class ProductMemoryAdmin(admin.ModelAdmin):
+    list_display = ['id', 'label']
+    list_display_links = ['label']
+
+
+@admin.register(ProductImageSet)
+class ProductImageSetAdmin(admin.ModelAdmin):
+    list_display = ['id', 'product_image']
+    list_display_links = ['product_image']
+
+
+@admin.register(CustomCharacter)
+class CustomCharacterAdmin(admin.ModelAdmin):
+    list_display = ['id', 'custom_character_name', 'description']
+    list_display_links = ['custom_character_name']
+
+
+@admin.register(CustomCharacterOption)
+class CustomCharacterOptionAdmin(admin.ModelAdmin):
+    list_display = ['id', 'custom_character']
+    list_display_links = ['custom_character']
+
+
+@admin.register(ProductVariantCustomCharacter)
+class ProductVariantCustomCharacterAdmin(admin.ModelAdmin):
+    list_display = ['id', 'product_variant', 'custom_character']
+    list_display_links = ['product_variant', 'custom_character']
+
--- backend/products/models.py
+++ backend/products/models.py
@@ -1,52 +1,145 @@
 from django.db import models
-from django.contrib.auth import get_user_model
 
-from accounts.models import Address
+class Company(models.Model):
+    name = models.CharField(max_length=150)
+    short_description = models.CharField(max_length=255)
+    logo = models.ImageField(upload_to='company_logo', null=True, blank=True)
 
+    class Meta:
+        verbose_name_plural = 'Companies'
 
-User = get_user_model()
+    def __str__(self):
+        return self.name
+    
+
+class Brand(models.Model):
+    company = models.ForeignKey(Company, on_delete=models.SET_NULL, null=True, blank=True)
+    name = models.CharField(max_length=150)
+
+    def __str__(self):
+        return self.name
+    
+
+class CompanyBrand(models.Model):
+    company = models.ForeignKey(Company, on_delete=models.CASCADE, null=True, blank=True)
+    brand = models.ForeignKey(Brand, on_delete=models.CASCADE, null=True, blank=True)
+
+    class Meta:
+        verbose_name_plural = 'BrandCompany'
+
+    def __str__(self):
+        return f"{self.brand} {self.company}"
 
 
 class Category(models.Model):
-    category_name = models.CharField(max_length=100)
-    parent = models.ForeignKey('self', null=True, blank=True, on_delete=models.SET_NULL)
+    name = models.CharField(max_length=150)
+    has_size = models.BooleanField(default=False, null=True, blank=True) # used for type rendering
+    has_memory = models.BooleanField(default=False, null=True, blank=True) # used for type rendering
+    has_color = models.BooleanField(default=False, null=True, blank=True) # used for type rendering
+    parent = models.ForeignKey(to="self", null=True, blank=True, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name_plural = 'Categories'
 
     def __str__(self):
-        return self.category_name
-
-
-class Vendor(models.Model):
-    user = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
-    company_name = models.CharField(max_length=150, null=True, blank=True)
-    address = models.ForeignKey(Address, null=True, blank=True, on_delete=models.SET_NULL)
-
-    def __str__(self):
-        return self.user
+        return self.name
 
 
 class Product(models.Model):
-    product_name = models.CharField(max_length=150)
+    name = models.CharField(max_length=150)
+    brand_company = models.ForeignKey(CompanyBrand, on_delete=models.SET_NULL, null=True, blank=True)
     short_description = models.CharField(max_length=255)
     full_description = models.TextField(null=True, blank=True)
-    price = models.DecimalField(max_digits=20, decimal_places=2)
-    vendor = models.ForeignKey(Vendor, null=True, on_delete=models.SET_NULL)
-
+    added_at = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+    produced_at = models.DateField(null=True, blank=True)
+    
     def __str__(self):
-        return self.product_name
+        return self.name
 
 
 class ProductCategory(models.Model):
-    product = models.ForeignKey(Product, null=True, on_delete=models.SET_NULL)
-    category = models.ForeignKey(Category, null=True, on_delete=models.SET_NULL)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name_plural = 'ProductCategories'
 
     def __str__(self):
-        return f"{self.category}: {self.product}"
+        return f"{self.category} {self.product}"
+    
+
+class ProductColor(models.Model):
+    name = models.CharField(max_length=150, null=True, blank=True)
+    code = models.CharField(max_length=8, null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.name} {self.code}"
+
+
+class ProductMemory(models.Model):
+    label = models.CharField(max_length=150)
+
+    def __str__(self):
+        return self.label
+
+
+class ProductSize(models.Model):
+    size = models.CharField(max_length=5)
+    roman_label = models.CharField(max_length=3)
+
+    def __str__(self):
+        return self.size
 
 
 class ProductImage(models.Model):
     image = models.ImageField(upload_to="product")
-    product = models.ForeignKey(Product, null=True, on_delete=models.SET_NULL)
-    uploaded_at = models.DateTimeField(auto_now=True)
+    alt_text = models.CharField(max_length=255, null=True, blank=True)
 
     def __str__(self):
-        return self.product
+        return f"{self.image}"
+    
+
+class ProductImageSet(models.Model):
+    product_image = models.ForeignKey(ProductImage, on_delete=models.SET_NULL, null=True)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.product}"
+    
+
+class CustomCharacter(models.Model):
+    custom_character_name = models.CharField(max_length=150)
+    description = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.custom_character_name
+    
+
+class CustomCharacterOption(models.Model):
+    custom_character = models.ForeignKey(CustomCharacter, on_delete=models.CASCADE)
+    option = models.CharField(max_length=150)
+
+    def __str__(self):
+        return f"{self.custom_character} {self.option}"
+    
+
+class ProductVariant(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    name = models.CharField(max_length=150)
+    color = models.ForeignKey(ProductColor, on_delete=models.SET_NULL, null=True, blank=True)
+    memory = models.ForeignKey(ProductMemory, on_delete=models.SET_NULL, null=True, blank=True)
+    size = models.ForeignKey(ProductSize, on_delete=models.SET_NULL, null=True, blank=True)
+    image_set = models.ForeignKey(ProductImageSet, on_delete=models.SET_NULL, null=True, blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return self.name
+    
+
+class ProductVariantCustomCharacter(models.Model):
+    product_variant = models.ForeignKey(ProductVariant, on_delete=models.SET_NULL, null=True)
+    custom_character = models.ForeignKey(CustomCharacter, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return f"{self.product_variant} {self.custom_character}"
